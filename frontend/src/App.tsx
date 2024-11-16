dae6b0f9219430162f6542af5a46b79d482ac558
Jack Rong <jackrong03@gmail.com>
2024-11-16 19:42:49 +0000

    use .env properly to set flashcards URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import sampleFlashcards from "./data/sample-flashcards.json";
 import { Flashcard } from "./types";
 import Flashcards from "./components/Flashcards/Flashcards";
 
 const App: React.FC = () => {
+  const flashcardsUrl = `${import.meta.env.VITE_BACKEND_URL}/v1/flashcards`;
+
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
 
   useEffect(() => {
     const fetchFlashcards = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/v1/flashcards");
+        const response = await axios.get(flashcardsUrl);
         return response.data;
-        // setFlashcards(response.data);
       } catch (error) {
         console.error('Error fetching flashcards: ', error);
 
-        // ! Use sample flashcards if backend isn't running
+        // Use sample flashcards if backend isn't running
         return sampleFlashcards;
       }
     };
@@ -27,7 +28,7 @@ const App: React.FC = () => {
         setFlashcards(flashcards);
         setIsLoading(false);
       })
-  }, []);
+  }, [flashcardsUrl]);
 
   // TODO: A loading spinner while flashcards is empty or loading
   return isLoading ? (
