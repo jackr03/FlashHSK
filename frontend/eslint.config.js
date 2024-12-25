539f3310ef40a6c984b911ab84cfc2b637dcc484
Jack Rong <jackrong03@gmail.com>
2024-12-25 19:26:37 +0000

    enforce use of semicolons

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,50 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import importPlugin from 'eslint-plugin-import';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'import': importPlugin,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
-      'quotes': [
-        'error',
-        'single',
+      quotes: [
+        "error",
+        "single",
         { avoidEscape: true, allowTemplateLiterals: true },
       ],
-      'import/order': [
-        'error',
+      "import/order": [
+        "error",
         {
           groups: [
-            ['builtin', 'external'],
-            ['internal'],
-            ['parent'],
-            ['sibling'],
-            ['index'],
+            ["builtin", "external"],
+            ["internal"],
+            ["parent"],
+            ["sibling"],
+            ["index"],
           ],
-          'newlines-between': 'always',
+          "newlines-between": "always",
         },
       ],
-      'import/newline-after-import': ['error', { count: 1 }],
-      'import/no-duplicates': 'error',
+      "import/newline-after-import": ["error", { count: 1 }],
+      "import/no-duplicates": "error",
+      semi: ["error", "always"],
     },
   },
-)
+);
