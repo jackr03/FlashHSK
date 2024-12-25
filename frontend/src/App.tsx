f7ab8518da19c2e5024207e6405d45c6805fe286
Jack Rong <jackrong03@gmail.com>
2024-12-25 19:01:08 +0000

    add load bar and spinner to flashcards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
+import { ClipLoader } from "react-spinners";
 import axios from "axios";
 import sampleFlashcards from "./data/sample-flashcards.json";
 import { Flashcard } from "./types";
@@ -8,11 +10,15 @@ const App: React.FC = () => {
   const backendURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
   const flashcardsUrl = `${backendURL}/v1/flashcards`;
 
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const loadingBarRef = useRef<LoadingBarRef>(null);
 
   useEffect(() => {
     const fetchFlashcards = async () => {
+      loadingBarRef.current?.start();
+
       try {
         const response = await axios.get(flashcardsUrl);
         return response.data;
@@ -20,23 +26,32 @@ const App: React.FC = () => {
         console.error('Error fetching flashcards: ', error);
         // Use sample flashcards if backend isn't running
         return sampleFlashcards;
+      } finally {
+        loadingBarRef.current?.complete();
+        setIsLoading(false);
       }
     };
 
     fetchFlashcards()
-      .then((flashcards) => {
-        setFlashcards(flashcards);
-        setIsLoading(false);
-      })
+      .then((flashcards) => setFlashcards(flashcards));
   }, [flashcardsUrl]);
 
-  // TODO: A loading spinner while flashcards is empty or loading
-  return isLoading ? (
-    <p>Loading...</p>
-  ) : (
-    <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
-      <Flashcards flashcards={flashcards} />
-    </div>
+  return (
+    <>
+      <LoadingBar
+        color="red"
+        height={4}
+        ref={loadingBarRef}
+      />
+
+      <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
+        {isLoading ? (
+          <ClipLoader color="red" size={50} />
+        ) : (
+          <Flashcards flashcards={flashcards}/>
+        )}
+      </div>
+    </>
   );
 };
 
