15bfb778915f262b3fa3283da726e0497038af46
Jack Rong <jackrong03@gmail.com>
2024-12-28 20:56:30 +0000

    refactor App.tsx and create FlashcardsPage, which is responsible for fetching flashcards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,7 @@
-import { useEffect, useRef } from 'react';
-import LoadingBar, { LoadingBarRef } from 'react-top-loading-bar';
-import { ClipLoader } from 'react-spinners';
-
-import Flashcards from './components/Flashcards/Flashcards';
-import useFetchFlashcards from './hooks/useFetchFlashcards.ts';
+import FlashcardsPage from './components/Flashcards/FlashcardsPage.tsx';
 
 const App: React.FC = () => {
-  const { flashcards, isLoading } = useFetchFlashcards();
-  const loadingBarRef = useRef<LoadingBarRef>(null);
-
-  useEffect(() => {
-    if (isLoading) {
-      loadingBarRef.current?.continuousStart();
-    } else {
-      loadingBarRef.current?.complete();
-    }
-  }, [isLoading]);
-
-  return (
-    <>
-      <LoadingBar color="red" height={4} ref={loadingBarRef} />
-
-      <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
-        {isLoading ? (
-          <ClipLoader color="red" size={50} />
-        ) : (
-          <Flashcards flashcards={flashcards} />
-        )}
-      </div>
-    </>
-  );
+  return <FlashcardsPage />;
 };
 
 export default App;
---
+++ frontend/src/components/Flashcards/FlashcardsPage.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+import LoadingBar, { LoadingBarRef } from 'react-top-loading-bar';
+import { ClipLoader } from 'react-spinners';
+
+import useFetchFlashcards from '../../hooks/useFetchFlashcards.ts';
+
+import Flashcards from './Flashcards.tsx';
+
+const FlashcardsPage: React.FC = () => {
+  const { flashcards, isLoading } = useFetchFlashcards();
+  const loadingBarRef = useRef<LoadingBarRef>(null);
+
+  useEffect(() => {
+    if (isLoading) {
+      loadingBarRef.current?.continuousStart();
+    } else {
+      loadingBarRef.current?.complete();
+    }
+  }, [isLoading]);
+
+  return (
+    <>
+      <LoadingBar
+        color="red"
+        height={4}
+        ref={loadingBarRef}
+        data-testid="test"
+      />
+
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        {isLoading ? (
+          <ClipLoader color="red" size={50} data-testid="test" />
+        ) : (
+          <Flashcards flashcards={flashcards} />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default FlashcardsPage;
